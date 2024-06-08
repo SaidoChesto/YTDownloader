d68ee5c3afb83307855f02618d4213062296fad1
SaidoChesto <diegommagalhaes07@gmail.com>
2024-06-08 19:39:51 -0300

    Outra versão que o Fernando e o Tiago ajudaram

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,14 +1,14 @@
 from pytube import YouTube, Playlist
 from pytube.innertube import _default_clients #     Solução do problema de restrição de idade
 _default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID_CREATOR"]#    Solução parte dois
-
+ 
 #   Barra de Progresso de 0 a 100% (Não foi copiada da internet, eu juro, confia)
 def on_progress(video_stream, total_size, bytes_remaining):
     total_size = video_stream.filesize
     bytes_downloaded = total_size - bytes_remaining
     percent = (bytes_downloaded / total_size) * 100
     print("\r" + "▌" * int(percent) + " " * (100 - int(percent)) + " {}%".format(int(percent)), end='')
-
+ 
 # "Interface"
 print("-------------------------")
 print("* Youtube Downloader *")
@@ -16,34 +16,40 @@ print("-------------------------")
 print("Qual tipo de Download?")
 print("1 - Video")
 print("2 - Music Playlist \n")
-
+ 
 while True:
-    opcao = int(input("Digite a opção: ")) # Seletor
-
+    opcao = input("Digite a opção: ") # Seletor
+ 
     # Filtros: res="1080p" | res="720p" | res="360p" | file_extension='mp4' | only_audio=True | .get_highest_resolution()
-
-    if opcao == 1:
+ 
+    if opcao == "1":
         link = input("Link: ")
-        print("\n")
-        UmVideo = YouTube(link, on_progress_callback=on_progress)# Definindo o link e Adicionando a barra de progresso
-        print(UmVideo.title)# Adicionando o titulo do video
-
-        VideoDownload = UmVideo.streams.filter(res="720p").first()
-        VideoDownload.download("/Users/diegu/OneDrive/Documentos/[Minhas Coisas]/[03] Midia/[07] Youtube Downloader/Videos")# Definindo o local de download, caso não tenha o local sera no msm do .py
-        print("\n")
+ 
+        try:
+          UmVideo = YouTube(link, on_progress_callback=on_progress)# Definindo o link e Adicionando a barra de progresso
+          print(UmVideo.title)# Adicionando o titulo do video
+ 
+          VideoDownload = UmVideo.streams.filter(res="720p").first()
+          VideoDownload.download("/Users/diegu/OneDrive/Documentos/[Minhas Coisas]/[03] Midia/[07] Youtube Downloader/Videos")# Definindo o local de download, caso não tenha o local sera no msm do .py
+        except:
+          print("\nLink inválido.\n")
+          continue
         break
-
-    elif opcao == 2:
+ 
+    elif opcao == "2":
         link = input("Link: ")
-        print("\n")
         pl = Playlist(link)
-
-        for v in pl:
-            print(v)
-            yt = YouTube(v, on_progress_callback=on_progress)
-            stream = yt.streams.filter(only_audio=True).first().download("/Users/diegu/OneDrive/Documentos/[Minhas Coisas]/[03] Midia/[07] Youtube Downloader/Musica")
-            print("\n")
+ 
+        try:
+          for v in pl:
+              print(v)
+              yt = YouTube(v, on_progress_callback=on_progress)
+              stream = yt.streams.filter(only_audio=True).first().download("/Users/diegu/OneDrive/Documentos/[Minhas Coisas]/[03] Midia/[07] Youtube Downloader/Musica")
+        except:
+          print("\nLink inválido.\n")
+          continue
         break
-
-    else: break
-    
+ 
+    else: 
+      print("\nInput inválido.\n")
+      continue
